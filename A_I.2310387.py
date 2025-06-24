3c5bb772f82233aeacfacc6bc172c5101085ee69
bangday09 <146575714+bangday09@users.noreply.github.com>
2025-06-24 09:38:30 +0700

    Add files via upload

---
+++ A_I.2310387.py
@@ -0,0 +1,4 @@
+nama = input("Masukkan nama Anda: ")
+universitas = input("Masukkan nama universitas Anda: ")
+nim = input("Masukkan NIM Anda: ")
+print(f"\nPerkenalkan, nama saya {nama}, saya kuliah di {universitas} dengan NIM {nim}.")
